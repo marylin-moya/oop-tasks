d07f055d627ffd3f09e4e4cbafbf003a22823eac
Marylin Moya <mmoya@pros.com>
2020-07-15 22:02:35 -0400

    practice 4

---
+++ src/main/java/practica4/Main.java
@@ -0,0 +1,11 @@
+package practica4;
+
+public class Main
+{
+    public static void main(String[] args) {
+        ListLandTransport trans = new ListLandTransport();
+        trans.addLand(new Bicycle("Cross", 1500, false, false));
+        trans.addLand(new Car("Audi", 20000, true, false));
+        trans.display();
+    }
+}
---
+++ src/main/java/practica4/Transport.java
@@ -0,0 +1,23 @@
+package practica4;
+
+public class Transport
+{
+    String name;
+    int price;
+
+    public Transport(String name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+}
